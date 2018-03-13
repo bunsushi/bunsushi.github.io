8de727df47028ee1f20781e0c471bb9b9f1f064d
Tegan Bunsu Ashby <tbunsu@gmail.com>
2018-03-13 15:47:36 -0400

    created featured project category

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,20 +1,29 @@
 $(document).ready(function () {
 
     var featuredProjects = [
+        {   
+            name: "NYT Article Search",
+            image: "assets/images/nyt-article-search.JPG",
+            url: "https://bunsushi.github.io/NYT-Article-Search",
+            featured: false
+        },
         {
             name: "Trivia Game",
             image: "assets/images/trivia-game.JPG",
-            url: "https://bunsushi.github.io/TriviaGame"
+            url: "https://bunsushi.github.io/TriviaGame",
+            featured: true
         },
         {
             name: "You Have a Gambling Problem",
             image: "assets/images/gambling-problem.JPG",
-            url: "https://bunsushi.github.io/Gambling-Problem"
+            url: "https://bunsushi.github.io/Gambling-Problem",
+            featured: true
         },
         {
             name: "Get Spaced",
             image: "assets/images/get-spaced.JPG",
-            url: "https://bunsushi.github.io/Get-Spaced-Hangman"
+            url: "https://bunsushi.github.io/Get-Spaced-Hangman",
+            featured: true
         }]
 
     // GENERATE FEATURED PROJECTS
@@ -44,9 +53,11 @@ $(document).ready(function () {
 
             $(featuredProjectLink).append(projectContainer)
 
-            $("#projects").append(featuredProjectLink);
-            // $("#projects").append(projectContainer);
-            $("#recent-projects").append(projectContainer);
+            $("#recent-projects").append(featuredProjectLink);
+
+            if (featuredProjects[i].featured === true) {
+                $("#projects").append(featuredProjectLink);
+            }
         }
     }
 
