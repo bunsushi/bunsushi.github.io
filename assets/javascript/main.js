a39d1f6d64ec8bb7daef088de8cf6f5cd7901def
bunsushi <tbunsu@gmail.com>
2018-04-05 15:12:06 -0400

    updated project images

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,68 +1,84 @@
 $(document).ready(function () {
 
+    // ATTN REWORK THIS CODE SO THAT DIV FADES IN ONCE YOU HIT IT.
     $(window).scroll(function() {
         var windowBottom = $(this).scrollTop() + $(this).innerHeight();
         $(".hideme").each(function() {
           /* Check the location of each desired element */
         var objectBottom = $(this).offset().top;
 
-          /* If the element is completely within bounds of the window, fade it in */
+          /* If the element is within bounds of the window, fade it in */
           if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-            if ($(this).css("opacity")==0) {$(this).fadeTo(1000,1);}
+            if ($(this).css("opacity")==0) {$(this).fadeTo(800, 1);}
           } else { //object goes out of view (scrolling up)
-            if ($(this).css("opacity")==1) {$(this).fadeTo(1000,0);}
+            if ($(this).css("opacity")==1) {$(this).fadeTo(800, 0);}
           }
         });
       }).scroll(); //invoke scroll-handler on page-load
 
 var featuredProjects = [
     {
+        name: "Cinemapp.r",
+        image: "assets/images/cinemappr.png",
+        url: "https://bunsushi.github.io/Cinemappr",
+        repository: "https://cinemappr.herokuapp.com",
+        featured: true,
+        description: "An interactive movie theater location and showtime listing application integrated with Google Maps.",
+        tags: ["HTML/CSS", "Bootstrap", "JavaScript", "jQuery", "Node.js", "Express", "Sass", "Google Maps API", "Gracenote API"]
+    },
+    {
         name: "SEPTA Trolley Tracker",
         image: "assets/images/trolley-tracker.png",
         url: "https://bunsushi.github.io/SEPTA-Trolley-Tracker",
+        repository: "https://github.com/bunsushi/SEPTA-Trolley-Tradker",
         featured: false,
-        description: "SEPTA Trolley Tracker is my dream transit time table, where all the trolleys arrive at a predictable time. The route number reflects the trolley's frequency. Thus we can expect the #13 Trolley to arrive at a given station every 13 minutes.",
-        tags: ["HTML5", "CSS3", "Bootstrap", "Javascript", "jQuery", "Firebase", "NoSQL"]
+        description: "SEPTA Trolley Tracker is my dream transit time table, where all the trolleys arrive at a predictable time.",
+        tags: ["HTML/CSS", "Bootstrap", "JavaScript", "jQuery", "Firebase", "NoSQL"]
     },
     {
         name: "Giphy RXN",
         image: "assets/images/giphy-rxn.png",
         url: "https://bunsushi.github.io/Giphy-RXN",
+        repository: "https://github.com/bunsushi/Giphy-RXN",
         featured: false,
-        description: "Giphy RXN is a simple search tool for discovering gifs using the Giphy API. Click on a suggested tag or create one of your own using the search bar. Clicking on a tag will display up to 10 gifs from Giphy. Searching for a tag will automatically display relevant gifs and add that tag to your searches. Giphy RXN has nine suggested tags with a science and technology theme to get you started, but the sky's limit.",
-        tags: ["HTML5", "CSS3", "Bootstrap", "Javascript", "jQuery", "API", "Ajax"]
+        description: "Giphy RXN is a simple search tool for discovering gifs using the Giphy API.",
+        tags: ["HTML/CSS", "Bootstrap", "JavaScript", "jQuery", "Giphy API"]
     },
     {
         name: "NYT Article Search",
         image: "assets/images/nyt-article-search.png",
         url: "https://bunsushi.github.io/NYT-Article-Search",
+        repository: "https://github.com/bunsushi/NYT-Article-Search",
         featured: false,
-        description: "An application to fetch 1-10 top articles for a search term using the New York Times Article Search API. The search filters are: keyword, start date, end date, and number of articles to retrieve. Each article pulled includes the headline information, date of publication, a short summary, and a link to the full article on nytimes.com.",
-        tags: ["HTML5", "CSS3", "Bootstrap", "Javascript", "jQuery", "API", "Ajax"]
+        description: "An application to fetch up to 10 top articles for a search term using the New York Times Article Search API.",
+        tags: ["HTML/CSS", "Bootstrap", "JavaScript", "jQuery", "NYT API"]
     },
     {
         name: "Trivia Game",
-        image: "assets/images/trivia-game.JPG",
+        image: "assets/images/trivia-game.png",
         url: "https://bunsushi.github.io/TriviaGame",
-        featured: true,
-        description: "The trivia game is composed of 5 questions, each with 4 potential answers. The player has 20 seconds to answer a question. After a player has submitted a guess or time runs out, the correct answer is displayed for 5 seconds. At the end of the game, the total number of correct answers is displayed and the player has the option to play again.",
-        tags: ["HTML5", "CSS3", "Bootstrap", "Javascript", "jQuery"]
+        repository: "https://github.com/bunsushi/TriviaGame",
+        featured: false,
+        description: "The trivia game is composed of 5 questions, each with 4 potential answers. The player has 20 seconds to answer a question.",
+        tags: ["HTML/CSS", "Bootstrap", "JavaScript", "jQuery"]
     },
     {
         name: "You Have a Gambling Problem",
-        image: "assets/images/gambling-problem.JPG",
+        image: "assets/images/gambling-problem.png",
         url: "https://bunsushi.github.io/Gambling-Problem",
-        featured: true,
-        description: "",
-        tags: ["HTML5", "CSS3", "Bootstrap", "Javascript", "jQuery"]
+        repository: "https://github.com/bunsushi/Gambling-Problem",
+        featured: false,
+        description: "A math guessing game. Choose one of four hidden cards to reveal its value and try to add up card values to match the randomly generated goal number.",
+        tags: ["HTML/CSS", "Bootstrap", "JavaScript", "jQuery"]
     },
     {
         name: "Get Spaced",
-        image: "assets/images/get-spaced.JPG",
+        image: "assets/images/get-spaced.png",
         url: "https://bunsushi.github.io/Get-Spaced-Hangman",
-        featured: true,
+        repository: "https://github.com/bunsushi/Get-Spaced-Hangman",
+        featured: false,
         description: "A vanilla JavaScript hangman game with a solar system objects theme. Comes with three missions: Planets, Solar System, and Milky Way.",
-        tags: ["HTML5", "CSS3", "Bootstrap", "Javascript"]
+        tags: ["HTML/CSS", "Bootstrap", "JavaScript"]
     }]
 
 // GENERATE FEATURED PROJECTS
@@ -98,7 +114,7 @@ function displayFeaturedProjects() {
     }
 }
 
-// GENERATE FEATURED PROJECTS
+// GENERATE OTHER PROJECTS
 function displayOtherProjects() {
     for (var i = 0; i < featuredProjects.length; i++) {
         var featuredProjectLink = $("<a>");
@@ -108,7 +124,7 @@ function displayOtherProjects() {
         var projectContainer = $("<div>");
         projectContainer.addClass("project-summary");
 
-        var projectTitle = $("<h2>");
+        var projectTitle = $("<h3>");
         projectTitle.text(featuredProjects[i].name);
 
         var projectDescription = $("<div>");
@@ -123,12 +139,14 @@ function displayOtherProjects() {
         $(featuredProjectLink).append(deployedButton);
 
 
-        $(projectContainer).append(projectTitle);
         $(projectContainer).append(displayFeaturedProjects);
+        $(projectContainer).append(projectTitle);        
         $(projectContainer).append(projectDescription);
         $(projectContainer).append(featuredProjectLink);
 
-        $("#other-projects").append(projectContainer);
+        if (featuredProjects[i].featured === false) {
+            $("#other-projects").append(projectContainer);
+        }
 
     }
 }
