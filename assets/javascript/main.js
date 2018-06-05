925e946f637a7c4f1101e6e3e84df85c95094fd6
bunsushi <tbunsu@gmail.com>
2018-06-05 11:10:00 -0400

    kandinsky logo

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,20 +1,41 @@
 $(document).ready(function () {
 
-    // ATTN REWORK THIS CODE SO THAT DIV FADES IN ONCE YOU HIT IT.
-    // $(window).scroll(function() {
-    //     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-    //     $(".hideme").each(function() {
-    //       /* Check the location of each desired element */
-    //     var objectBottom = $(this).offset().top;
-
-    //       /* If the element is within bounds of the window, fade it in */
-    //       if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-    //         if ($(this).css("opacity")==0) {$(this).fadeTo(800, 1);}
-    //       } else { //object goes out of view (scrolling up)
-    //         if ($(this).css("opacity")==1) {$(this).fadeTo(800, 0);}
-    //       }
-    //     });
-    //   }).scroll(); //invoke scroll-handler on page-load
+// Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function (event) {
+    // On-page links
+    if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+    ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top - 100
+            }, 600, function () {
+                // Callback after animation
+                // Must change focus!
+                var $target = $(target);
+                $target.focus();
+                if ($target.is(":focus")) { // Checking if the target was focused
+                    return false;
+                } else {
+                    // $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                    $target.focus(); // Set focus again
+                };
+            });
+        }
+    }
+});
 
 var featuredProjects = [
     {
